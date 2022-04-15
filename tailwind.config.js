181ff8412dc1769ae07472034b8519e4941e9b57
foysal <foysalf652@gmail.com>
2022-04-16 00:52:39 +0600

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  // content: ["./src/**/*.{html,css,js}"],
   content: [
     './src/**/*.{html,js}',
     './dist/**/*.{html,js}',
     './index.html',
     './next.html',
- 
   ],
+  
   darkMode: 'media',
   theme: {
     extend: {},
