30fce2826b1d2c1a8ba90d989451e61665c2d200
foysal <foysalf652@gmail.com>
2022-04-16 00:35:51 +0600

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // content: ["./src/**/*.{html,css,js}"],
+  content: [
+    './src/**/*.{html,js}',
+    './dist/**/*.{html,js}',
+    './index.html',
+ 
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
